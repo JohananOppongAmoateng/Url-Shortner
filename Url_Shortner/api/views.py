ad2a3ef9e1b83985e891659b5ac4edcf337596c3
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-07-26 03:54:02 +0000

    view function

--- Url_Shortner/api/views.py
+++ Url_Shortner/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status,generics
+from rest_framework.views import APIView
+from django.http import HttpResponsePermanentRedirect
+
+from .models import Link
+from .serializers import LinkSerializers
 
 # Create your views here.
+
+class ShortenUrl(generics.CreateAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+
+
+
+def redirect_to_long_url(self,request,short_identifier):
+    url = get_object_or_404(Link, shorten_url=short_identifier)
+    return HttpResponsePermanentRedirect(url.target_url) 
+        
+
+
