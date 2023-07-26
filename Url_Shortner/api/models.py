d0d4271a6f82276bdcf53e5ca2d69d4365e752cb
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-07-26 03:17:46 +0000

    added Link model

--- Url_Shortner/api/models.py
+++ Url_Shortner/api/models.py
@@ -1,4 +1,11 @@
 from django.db import models
-
+from .managers import ActiveLinkManager
 # Create your models here.
 
+class Link(models.Model):
+    target_url = models.URLField(max_length=250)
+    description = models.CharField(max_length=255)
+    created_date = models.DateField(auto_now=True)
+    active = models.BooleanField(default=True)
+    objects = models.Manager()
+    public = ActiveLinkManager()
