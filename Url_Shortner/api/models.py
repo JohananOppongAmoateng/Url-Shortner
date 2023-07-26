b83614ba699cd4b769e22e4964a2acf52d8f8e4d
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-07-26 03:53:13 +0000

    added shorten ur;

--- Url_Shortner/api/models.py
+++ Url_Shortner/api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from .managers import ActiveLinkManager
 # Create your models here.
-
+from .utils import generate_random_id
 class Link(models.Model):
     target_url = models.URLField(max_length=250)
     description = models.CharField(max_length=255)
     created_date = models.DateField(auto_now=True)
+    shorten_url = models.CharField(default=generate_random_id())
     active = models.BooleanField(default=True)
     objects = models.Manager()
-    public = ActiveLinkManager()
